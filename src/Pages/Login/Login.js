922424fb82940eebf5460806ea7a8d2db3d2287e
f4faysal <f4faysals@gmail.com>
2023-04-05 00:39:38 +0600

    google login setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -37,6 +37,7 @@ const Login = () => {
   const handelSignInWithGoogle = () => {
     signInWithGoogle().then((result) => {
       setAuthToken(result);
+      toast.success("Logine Success..");
       navigate(from, { replace: true });
 
       console.log("Google user :>> ", result);
@@ -45,10 +46,12 @@ const Login = () => {
 
   return (
     <div className="flex flex-col justify-center items-center pt-8  gap-5  bg-[url('https://tenleemedia.com/wp-content/uploads/2020/10/tenleemedia-works.jpg')] h-screen w-full bg-cover">
-      <Logo/>
+      <Logo />
       <div className="flex flex-col max-w-md p-5 rounded-md sm:p-8  bg-white/50  text-gray-900 ">
         <div className="text-center">
-          <p className="text-sm text-gray-600">Sign in to access your account</p>
+          <p className="text-sm text-gray-600">
+            Sign in to access your account
+          </p>
         </div>
         <form
           onSubmit={handelSubmit}
@@ -57,24 +60,7 @@ const Login = () => {
           className="space-y-4 ng-untouched ng-pristine ng-valid"
         >
           <div className="space-y-4">
-            {/* <div>
-              <label htmlFor="email" className="block mb-2 text-sm">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                required
-                placeholder="Enter Your Email Here"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-primary bg-gray-200 text-gray-900"
-                data-temp-mail-org="0"
-              />
-            </div> */}
             <div className="form-control">
-              <label className="label">
-                {/* <span className="label-text">Your Email</span> */}
-              </label>
               <label className="input-group input-group-vertical">
                 <span>Email</span>
                 <input
@@ -88,26 +74,7 @@ const Login = () => {
               </label>
             </div>
 
-            {/* <div>
-              <div className="flex justify-between">
-                <label htmlFor="password" className="text-sm mb-2">
-                  Password
-                </label>
-              </div>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                required
-                placeholder="*******"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-primary bg-gray-200 text-gray-900"
-              />
-            </div> */}
-
             <div className="form-control">
-              {/* <label className="label">
-                <span className="label-text">Your Password</span>
-              </label> */}
               <label className="input-group input-group-vertical">
                 <span>Password</span>
                 <input
@@ -136,35 +103,33 @@ const Login = () => {
             Forgot password?
           </button>
         </div>
-        <div className="flex items-center pt-4 space-x-1">
+        <div className="flex items-center  space-x-1">
           <div className="flex-1 h-px sm:w-16 dark:bg-white"></div>
-          <p className="px-3 text-sm dark:text-gray-600">
-            Login with custom Accounts
-          </p>
+          <div className="flex justify-center space-x-4">
+            <button
+              onClick={handelSignInWithGoogle}
+              aria-label="Log in with Google"
+              className="p-1 rounded-sm"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+                className="w-5 h-5 fill-current"
+              >
+                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+              </svg>
+            </button>
+          </div>
           <div className="flex-1 h-px sm:w-16 dark:bg-white"></div>
         </div>
-        {/* <div className="flex justify-center space-x-4">
-          <button
-            onClick={handelSignInWithGoogle}
-            aria-label="Log in with Google"
-            className="p-3 rounded-sm"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-              className="w-5 h-5 fill-current"
-            >
-              <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-            </svg>
-          </button>
-        </div> */}
+
         <p className="px-6 text-sm text-center text-gray-500">
           Don't have an account yet?{" "}
           <Link
             to="/signup"
             className="hover:underline text-[#2498ad] hover:text-secondary font-semibold"
           >
-            Register 
+            Register
           </Link>
           .
         </p>
